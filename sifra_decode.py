92e18585dece964255c19ae8af7961a71327a193
Stlpikjunior <113450933+Stlpikjunior@users.noreply.github.com>
2022-11-09 18:51:14 +0100

    Add files via upload

---
+++ sifra_decode.py
@@ -0,0 +1,12 @@
+def sifra(ret:str,key:str):
+    y = 0
+    z = 0
+    n = ''
+    for i in ret:
+        w = y % len(key)
+        x = chr((ord(i)-97 -ord(key[w])+97)%26 +97 )
+        y += 1
+        n += x
+    print(n)
+
+sifra('kcsccqim','kvet')
